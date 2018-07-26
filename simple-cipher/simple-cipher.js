ccae8caaab0693b99f52d51befe97a052df7d757
plato721 <matt.ewell@gmail.com>
2018-07-26 10:30:22 -0600

    Substituion cipher functional

--- simple-cipher/simple-cipher.js
+++ simple-cipher/simple-cipher.js
@@ -1,8 +1,9 @@
 'use strict'
 
 class Year {
-  constructor () {
-    this.key = this.generateKey()
+  constructor (key) {
+    this.key = key || this.generateKey()
+    this.baselineCode = 'a'.charCodeAt(0)
   }
 
   generateKey () {
@@ -11,6 +12,33 @@ class Year {
       let mappedAscii = min.charCodeAt(0) + offset
       return String.fromCharCode(mappedAscii)
     }).join('')
+  }
+
+  keyOffset (index) {
+    let adjustedIndex = index % this.key.length
+    return this.key.charCodeAt(adjustedIndex) - this.baselineCode
+  }
+
+  encodeLetter (letter, index) {
+    let rawEncoded = letter.charCodeAt(0) + this.keyOffset(index)
+    while (rawEncoded - this.baselineCode > 25) { rawEncoded -= 26 }
+    return String.fromCharCode(rawEncoded)
+  }
+
+  decodeLetter (letter, index) {
+    let rawDecoded = letter.charCodeAt(0) - this.keyOffset(index)
+    while (rawDecoded < this.baselineCode) { rawDecoded += 26 }
+    return String.fromCharCode(rawDecoded)
+  }
+
+  encode (plain) {
+    return plain.split('').map((char, i) => this.encodeLetter(char, i))
+      .join('')
+  }
+
+  decode (encoded) {
+    return encoded.split('').map((char, i) => this.decodeLetter(char, i))
+      .join('')
   }
 }
 
