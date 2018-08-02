2af1436d057080de29a15edb6d4f3876c7096ebf
plato721 <matt.ewell@gmail.com>
2018-08-02 16:33:11 -0600

    Add solution to binary

--- binary/binary.js
+++ binary/binary.js
@@ -1,8 +1,23 @@
 'use strict'
 
 class Binary {
+  constructor (binNum) {
+    this.binNum = binNum
+  }
+
+  isValid () {
+    return this.binNum.match(/^[01]+$/)
+  }
+
   toDecimal () {
-    return 0
+    if (!this.isValid()) { return 0 }
+
+    let maxPower = this.binNum.length - 1
+    return this.binNum.split('').reduce((sum, digitChar, index) => {
+      let power = maxPower - index
+      let digit = Number.parseInt(digitChar)
+      return sum += digit * Math.pow(2, power)
+    }, 0)
   }
 }
 
